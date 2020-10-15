2caa825612543b1484feac74f4dcdf1aa79dbfbe
Jec Castanos <jec.castanos@gmail.com>
2020-10-15 22:09:31 +0800

    App mount testing

--- app/App.js
+++ app/App.js
@@ -7,42 +7,44 @@ import User from './components/User';
 import Alert from './components/Alert';
 import logo from '../assets/logo.svg';
 
-const App = ({ state, fetchUsers }) => {
-    const { users, loading, message } = state;
+export const App = ({ state, fetchUsers }) => {
+  const { users, loading, message } = state;
 
-    const loadUsers = () => {
-        fetchUsers();
-    }
+  const loadUsers = () => {
+    fetchUsers();
+  };
 
-    const renderUser = () => {
-        if (message) return <Alert message={message} />
-        return <section className="users">
-            {users.length
-                ? users.map(user => <User key={user.id} user={user} />)
-                : null
-            }
-        </section>
-    }
-
+  const renderUser = () => {
+    if (message) return <Alert message={message} />;
     return (
-        <main className="t-center">
-            <img src={logo} className="logo" alt="logo" />
-            <h1>Click Fetch & let Saga does the rest.</h1>
-            <button onClick={loadUsers}>Fetch</button>
-            {loading ? <Alert message="Loading..." /> : renderUser()}
-        </main>
-    )
-}
+      <section className="users">
+        {users.length
+          ? users.map((user) => <User key={user.id} user={user} />)
+          : null}
+      </section>
+    );
+  };
+
+  return (
+    <main className="t-center">
+      <img src={logo} className="logo" alt="logo" />
+      <h1>Click Fetch & let Saga does the rest.</h1>
+      <button onClick={loadUsers}>Fetch</button>
+      {loading ? <Alert message="Loading..." /> : renderUser()}
+    </main>
+  );
+};
 
 App.propTypes = {
-    state: PropTypes.object.isRequired,
-    fetchUsers: PropTypes.func.isRequired,
-}
+  state: PropTypes.object.isRequired,
+  fetchUsers: PropTypes.func.isRequired,
+};
 
-const mapStateToProps = state => ({
-    state: state.userReducer
-})
+const mapStateToProps = (state) => ({
+  state: state.userReducer,
+});
 
-const mapDispatchToProps = dispatch => bindActionCreators({ fetchUsers }, dispatch);
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators({ fetchUsers }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
