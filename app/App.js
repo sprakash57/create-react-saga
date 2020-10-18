951a28923abb335029e65cd1b180fcf6b2c72487
Toshiuk <nara_toshiraka@hotmail.com>
2020-10-18 21:44:37 +0100

    Fix issue #11 tri-linear logos at the top

--- app/App.js
+++ app/App.js
@@ -5,7 +5,9 @@ import { bindActionCreators } from 'redux';
 import { fetchUsers } from './actions';
 import User from './common/User';
 import Alert from './common/Alert';
-import logo from '../assets/logo.svg';
+import reactLogo from '../assets/reactLogo.svg';
+import webpackLogo from '../assets/webpackLogo.svg';
+import reduxLogo from '../assets/reduxLogo.svg';
 import { Button } from './components/Button';
 
 export const App = ({ state, fetchUsers }) => {
@@ -28,7 +30,17 @@ export const App = ({ state, fetchUsers }) => {
 
   return (
     <main className="t-center">
-      <img src={logo} className="logo" alt="logo" />
+      <ul className="logos">
+        <li>
+          <img src={webpackLogo} className="logo" alt="webpackLogo" />
+        </li>
+        <li>
+          <img src={reactLogo} className="logo" alt="reactLogo" />
+        </li>
+        <li>
+          <img src={reduxLogo} className="logo" alt="reduxLogo" />
+        </li>
+      </ul>
       <h1>Click Fetch & let Saga does the rest.</h1>
       <Button label="Fetch" onClick={loadUsers} />
       {loading ? <Alert message="Loading..." /> : renderUser()}
