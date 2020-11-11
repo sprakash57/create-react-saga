924147bd87db9dd058c5e94dfc04efbccc131739
Sunny Prakash <sunny.prakashgm@gmail.com>
2020-11-11 23:26:35 +0530

    Updated to version 0.7.2

--- src/main.js
+++ src/main.js
@@ -3,14 +3,37 @@ import fs from 'fs';
 import ncp from 'ncp';
 import path from 'path';
 import { promisify } from 'util';
+import execa from 'execa';
+import Listr from 'listr';
+import { projectInstall } from 'pkg-install';
+import boxen from 'boxen';
 
 const access = promisify(fs.access);
 const copy = promisify(ncp);
+const boxenOptions = {
+    padding: 1,
+    margin: 1,
+    borderStyle: "round",
+    borderColor: "green"
+};
 
 const copyCoreFiles = async (options) => {
+    const packageJsonPath = path.resolve(path.dirname(__filename), '../core', 'package.json');
+    const raw = fs.readFileSync(packageJsonPath);
+    const packageJson = JSON.parse(raw);
+    packageJson.name = options.directory;
+    fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
     const { source, target } = options;
     return copy(source, target, { clobber: false });
 };
+
+const initGit = async (options) => {
+    const result = await execa('git', ['init'], {
+        cwd: options.target
+    });
+    if (result.failed) return Promise.reject(new Error("Failed to initialize git"));
+    return;
+}
 
 export const createProject = async (options) => {
     const source = path.resolve(path.dirname(__filename), '../core');
@@ -22,13 +45,28 @@ export const createProject = async (options) => {
     try {
         await access(source, fs.constants.R_OK);
     } catch (error) {
-        console.error("%s Invalid template name", chalk.red.bold("ERROR"));
+        console.error("%s: Invalid template name", chalk.red.bold("ERROR"));
         process.exit(1);
     }
 
-    console.log('Copying core files...');
-    await copyCoreFiles(options);
+    const tasks = new Listr([
+        {
+            title: 'Copying project files...',
+            task: () => copyCoreFiles(options),
+        },
+        {
+            title: 'Initializing git...',
+            task: () => initGit(options),
+            enabled: () => options.git,
+        },
+        {
+            title: 'Installing dependencies...',
+            task: () => projectInstall({ cwd: options.target })
+        },
+    ]);
 
-    console.log("%s Project is ready", chalk.green.bold("DONE"));
+    await tasks.run();
+    console.log("%s: Project is ready.", chalk.green.bold("DONE"));
+    console.log(boxen(chalk.green("Happy Coding with Webpack-React-Saga !!"), boxenOptions))
     return true;
 };
