95f382d70142e736d622f359bdf2e6de31b9a695
Sunny Prakash <sunny.prakashgm@gmail.com>
2020-11-19 21:10:19 +0530

    deleted non-doc related file

--- core/app/constants.js
+++
@@ -1,5 +0,0 @@
-export const USERS = 'https://jsonplaceholder.typicode.com/users';
-export const FETCH_USERS = 'FETCH_USERS';
-export const FETCH_USERS_RESOLVED = 'FETCH_USERS_RESOLVED';
-export const FETCH_USERS_REJECTED = 'FETCH_USRES_REJECTED';
-export const ERROR = 'Hmm... Looks like there is some network issue!!';
