ce15f56647013c34d177bf3db6e453975fe09e76
Sunny Prakash <sunny.prakashgm@gmail.com>
2021-01-16 21:16:14 +0530

    package size has been reduced

--- core/.storybook/main.js
+++ core/.storybook/main.js
@@ -5,7 +5,6 @@ module.exports = {
     "../app/components/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
-    "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-scss"
   ]
--- core/setupTests.js
+++ core/setupTests.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Enzyme, { mount, render } from "enzyme";
+import Enzyme, { mount, shallow } from "enzyme";
+import { render, cleanup, screen, fireEvent } from '@testing-library/react';
 import Adapter from "enzyme-adapter-react-16";
 import toJson from 'enzyme-to-json';
 
@@ -7,5 +8,9 @@ Enzyme.configure({ adapter: new Adapter() });
 
 global.mount = mount;
 global.render = render;
+global.shallow = shallow;
+global.cleanup = cleanup;
+global.screen = screen;
+global.fireEvent = fireEvent;
 global.React = React;
 global.toJson = toJson;
