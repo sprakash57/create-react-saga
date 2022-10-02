2dfefaa5a32181ddd4aa9206b6a6db58ee5a4763
Indrani Biswas <indranibiswas@Indranis-MacBook-Air.local>
2022-10-02 17:08:22 +0200

    refactor: update javascript template with new styles

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchUsers } from './store/users';
 import wrs from '../assets/wrs.png';
-import { Button, Alert, User } from './components';
+import { Button, User } from './components';
+import appInfo from '../package.json';
 
 const App = () => {
   const { users, isLoading, message } = useSelector(state => state.usersReducer);
   const dispatch = useDispatch();
 
-  const loadUsers = () => {
-    dispatch(fetchUsers());
-  };
-
   const renderUser = () => {
-    if (message) return <Alert message={message} data-testid="alert" />;
+    if (isLoading) return <h2>Loading...</h2>
+    if (message) return <h2>{message}</h2>;
     return (
       <section className="users">
         {users.length
@@ -24,15 +22,22 @@ const App = () => {
   };
 
   return (
-    <main className="t-center">
-      <img src={wrs} className="logo" alt="webpackLogo" />
-      <h1 className="t-header">
-        <span>Click </span>
-        <Button label="Fetch" variant="success" onClick={loadUsers} data-testid="succss-btn" />
-        <span> & let Saga do the rest. </span>
-        <span>Explore <a href="https://sprakash57.github.io/create-react-saga" rel="noopener noreferrer" target="_blank">Docs</a> for more.</span>
-      </h1>
-      {isLoading ? <Alert message="Loading..." /> : renderUser()}
+    <main className='main'>
+      <nav className='main__navbar'>
+        <div className='main__brand'>
+          <h1>Create React Saga</h1>
+          <sub>v{appInfo.version}</sub>
+        </div>
+        <div>
+          <a className='links card' href="https://github.com/sprakash57/create-react-saga" rel="noopener noreferrer" target="_blank">GitHub</a>
+          <a className='links card' href="https://sprakash57.github.io/create-react-saga" rel="noopener noreferrer" target="_blank">Docs</a>
+        </div>
+      </nav>
+      <img src={wrs} className="main__logo" alt="create-react-saga" />
+      <section>
+        <Button label="Click to run Saga" variant="success" onClick={() => dispatch(fetchUsers())} data-testid="succss-btn" />
+      </section>
+      {renderUser()}
     </main>
   );
 };
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,2 @@
 export { default as Button } from './Button';
-export { default as Alert } from './Alert';
 export { default as User } from './User';
